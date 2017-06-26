d9eab5a42db6232b66018decb473a01b3aff9235
Vladimir Polyakov <vladimir.polyakov.1988@gmail.com>
2017-06-26 23:40:33 +0300

    update tests for run in docker

--- integration_tests/pipeline_builder_functional_test.js
+++ integration_tests/pipeline_builder_functional_test.js
@@ -1,4 +1,5 @@
 const assert = require('assert'),
+  sauceConnectLauncher = require('sauce-connect-launcher'),
   test = require('selenium-webdriver/testing'),
   webdriver = require('selenium-webdriver'),
   pixelmatch = require('pixelmatch'),
@@ -6,25 +7,43 @@ const assert = require('assert'),
   SauceLabs = require('saucelabs'),
   fs = require('fs'),
   svg2png = require('svg2png'),
-  path = require('path');
+  path = require('path'),
   username = process.env.SAUCE_USERNAME,
   accessKey = process.env.SAUCE_ACCESS_KEY,
   saucelabs = new SauceLabs({
     username: username,
-    password: accessKey
+    password: accessKey,
   });
 
 test.describe('Pipelin Builder JS functional test', function () {
-  this.timeout(600000);
+  this.timeout(9999999);
+
+  test.before(function(done) {
+    sauceConnectLauncher({
+      username: username,
+      accessKey: accessKey,
+    }, function (err) {
+      if (err) {
+        console.error(err.message);
+      }
+     console.log("Sauce Connect ready");
+    }, done);
+  });
+
+  // sauceConnectLauncher.sauceConnectProcess.close(function () {
+  //   console.log("Closed Sauce Connect process");
+  // });
+
 
   var driver;
 
   test.beforeEach(function () {
+    saucelabs.getTunnel(sauceConnectLauncher.sauceConnectProcess.tunnelId);
+    console.log(sauceConnectLauncher.sauceConnectProcess.tunnelId);
     var browser = 'chrome',
       version = '43.0',
       platform = 'Windows 7',
       screenResolution = '1920x1080',
-      tunnel_identifier = process.env.TRAVIS_JOB_NUMBER,
       server = "http://" + username + ":" + accessKey +
         "@ondemand.saucelabs.com:80/wd/hub";
 
@@ -35,8 +54,7 @@ test.describe('Pipelin Builder JS functional test', function () {
       'platform': platform,
       'version': version,
       'username': username,
-      'accessKey': accessKey,
-      'tunnel-identifier': tunnel_identifier
+      'accessKey': accessKey
     }).usingServer(server).build();
     driver.getSession().then(function (sessionid) {
       driver.sessionID = sessionid.id_;
@@ -55,15 +73,21 @@ test.describe('Pipelin Builder JS functional test', function () {
     }, done);
   });
 
+ //  test.after(function(done) {
+ //    sauceConnectLauncher({
+ //      sauceConnectProcess.close(function () {
+ //      console.log("Closed Sauce Connect process")});
+ // }, done);
+ //  });
+
   function doneReading(golden, screenshot) {
     var diff = pixelmatch(golden, screenshot, diff, 1920, 899);
     return diff;
   }
 
-  const PB_APP_URL = 'http://172.22.3.10:8081/';
+  const PB_APP_URL = 'http://pb.opensource.epam.com/';
   const REFERENCE_TMP_PNG = 'buffer.png';
   const REFERENCE_TMP_SVG = 'buffer.svg';
-  const WDL_SCRIPT = 'test.wdl';
   const CASES_PATH = './integration_tests/cases/';
   var contents = fs.readFileSync(path.resolve(path.join(CASES_PATH + 'test-cases.json')));
   var test_cases = JSON.parse(contents);
@@ -72,12 +96,12 @@ test.describe('Pipelin Builder JS functional test', function () {
 
     test.it(test_case.name, function () {
       driver.get(PB_APP_URL);
-      request(test_case.wdl_url).pipe(fs.createWriteStream(WDL_SCRIPT));
-      console.log(wdl_cont);
-      wdl_cont = fs.readFileSync(WDL_SCRIPT);
-      driver.wait(until.elementLocated(webdriver.By.xpath('//*[@id="btn-build"]')), 10000, 'Page wasn\'t loaded in time');
+      // request(test_case.wdl_url).pipe(fs.createWriteStream(WDL_SCRIPT));
+      // console.log(wdl_cont);
+      var wdlCont = getResponse(test_case.wdl_url, 'GET');
+      driver.wait(until.elementLocated(webdriver.By.xpath('//*[@id="btn-build"]')), 9999999, 'Page wasn\'t loaded in time');
       driver.executeScript("document.getElementById('txt-script').value = '" +
-        wdl_cont + "';").then(function () {
+        wdlCont + "';").then(function () {
         driver.findElement(webdriver.By.xpath('//*[@id="btn-build"]')).click();
         driver.executeScript('var svg = document.querySelector("svg").parentNode.innerHTML; return svg;').then(function (return_value) {
           fs.writeFileSync(REFERENCE_TMP_SVG, return_value);
