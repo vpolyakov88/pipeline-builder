893f7365ad351ac1c6d69d7127c8313dd81ce8e9
Vladimir Polyakov <vladimir.polyakov.1988@gmail.com>
2017-06-30 18:08:03 +0300

    tunell waiter added

---
+++ integration_tests/tunnel_wait.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+response=[]
+val=[]
+while [[ $response == $val ]]; do
+response=$(curl -u $1:$2 -sb -H "Accept: application/json" "https://saucelabs.com/rest/v1/$1/tunnels")
+done
+sleep 20
