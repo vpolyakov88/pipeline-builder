d8177d10b8f3289df43d68e29b6f90b90cf05501
Vladimir Polyakov <vladimir.polyakov.1988@gmail.com>
2017-06-30 20:11:51 +0300

    update bash script that wait tunnell

--- integration_tests/tunnel_wait.sh
+++ integration_tests/tunnel_wait.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 response=[]
-val=[]
-while [[ $response == $val ]]; do
-response=$(curl -u $1:$2 -sb -H "Accept: application/json" "https://saucelabs.com/rest/v1/$1/tunnels")
+val="\"status\": \"running\""
+while [[ $response != *$val* ]]; do
+response=$(curl -u $1:$2 -sb -H "Accept: application/json" "https://saucelabs.com/rest/v1/$1/available_tunnels?full=true")
 done
-sleep 15
